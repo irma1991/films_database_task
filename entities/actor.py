2edc93bf1bc31093cd2d2802e2dbfb79d95cdbcf
Moksleivis <moksleivis@KITM.local>
2020-01-24 11:02:14 +0200

    pradzia

---
+++ entities/actor.py
@@ -0,0 +1,5 @@
+class Actor:
+
+    def __init__(self, actorsId, name):
+        self.actorsId = actorsId
+        self.name = name
---
+++ entities/movie.py
@@ -0,0 +1,10 @@
+class Movies:
+
+    def __init__(self, moviesId, movie_name, release_date, rating, genre, box_office_name, studioName):
+        self.moviesId = moviesId
+        self.movie_name = movie_name
+        self.release_date = release_date
+        self.rating = rating
+        self.genre = genre
+        self.box_office_name = box_office_name
+        self.studioName = studioName
